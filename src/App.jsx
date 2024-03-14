8e7add653ef926723bfed473ea9dde5455b996ab
Lutfaakter <lutfanur20@gmail.com>
2024-03-14 13:08:43 +0000

    added fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-4xl'>Knowledge cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
