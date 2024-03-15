95ac0588f3e6df91e9c7a1a48a4a2be46e778b7f
Lutfaakter <lutfanur20@gmail.com>
2024-03-15 09:30:26 +0000

    added event handler

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, author, author_img, posted_date, reading_time } = blog;
+    return (
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex mb-4'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button
+                        onClick={() => handleAddToBookmark(blog)}
+                        className='ml-2 text-2xl text-red-600'
+                    ><FaBookmark></FaBookmark></button>
+                </div>
+
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func
+}
+export default Blog;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl font-bold'>Knowledge cafe</h1>
-        </div>
+        <header className='flex justify-between items-center p-4  border-b-2 md:flex max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
